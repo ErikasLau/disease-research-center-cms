9ad7adbd1203885b51c40e1e81cd81720c5a86be
Erikas Laužadis <elauzadis@gmail.com>
2024-10-29 23:22:36 +0200

    Create localization files to better translate default text

---
+++ lang/lt/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | outcome such as failure due to an invalid password / reset token.
+    |
+    */
+
+    'reset' => 'Jūsų slaptažodis nustatytas iš naujo.',
+    'sent' => 'El. paštu išsiuntėme slaptažodžio nustatymo iš naujo nuorodą.',
+    'throttled' => 'Palaukite prieš bandydami dar kartą.',
+    'token' => 'Slaptažodžio nustatymo iš naujo raktas yra neteisingas.',
+    'user' => "Negalime rasti naudotojo su šiuo el. pašto adresu.",
+
+];
